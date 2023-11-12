cc0be53171b44cf196fddd90b6cc9414404f2992
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-11-12 20:31:53 -0300

    modificando a homePage no index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Link from "next/link"
+import { HomeNavBar } from "./homeScreen/homeNavBar/homeNavBar"
+import { HomeCards } from "./homeScreen/homeCards/homeCards"
+import styles from "./styles/index.module.css"
 
 export default function Main() {
     return(
-        <div>
-            <h1>Tela inicial</h1>
-            <Link href="/legendsPage">LegendsPage</Link>
+        <div className={styles.homePage}>
+            <HomeNavBar />
+            <HomeCards />
         </div>
     )
 }
