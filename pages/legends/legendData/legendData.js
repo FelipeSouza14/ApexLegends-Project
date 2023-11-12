24363df70ee4c0b73fb7693d8d28be0496f53390
IsaKaillany <isakaillany123@gmail.com>
2023-11-11 22:18:48 -0300

    criação da página legendData

---
+++ pages/legends/legendData/legendData.js
@@ -0,0 +1,9 @@
+export default function LegendData() {
+    return(
+        <div>
+            <h1>Tela de dados da lenda</h1>
+        </div>
+    )
+}
+
+//Precisará receber um parâmetro para saber qual lenda foi clicada
