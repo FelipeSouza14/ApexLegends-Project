01668e1b6372ea4b9610df233d91b1b75764de5e
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-11-13 23:34:26 -0300

    adicionando o js em mapsData

---
+++ pages/maps/mapsData/mapsData.js
@@ -0,0 +1,22 @@
+import styles from "./mapsData.module.css";
+
+export function mapsData (){
+    return (
+        <div className={styles.divMaps}>
+            <div className={styles.mapsData}>
+                <div className={styles.headDiv}>
+                    <h1 className={styles.mapsName}>Nome</h1>
+                    <h2 className={styles.favoriteIcon}>ICONE</h2>
+                </div>
+                <img className={styles.mapsImage} src={"/assets/mapas/WORLDS-EDGE/worlds-edge-official.jpg"} />
+            </div>
+            <div className={styles.mapsData}>
+                <div className={styles.headDiv}>
+                    <h1 className={styles.mapsName}>Nome</h1>
+                    <h2 className={styles.favoriteIcon}>ICONE</h2>
+                </div>
+                <img className={styles.mapsImage} src={"/assets/mapas/BROKEN-MOON/broken-moon-official.jpg"} />
+            </div>
+        </div>
+    )
+}
--- pages/maps/mapsPage.js
+++ pages/maps/mapsPage.js
@@ -1,24 +1,10 @@
 import styles from "./mapsPage.module.css";
+import { mapsData } from "./mapsData/mapsData";
 
 export default function MapsPage(){
     return (
         <div className={styles.mapsPage}>
-            <div className={styles.divMaps}>
-                <div className={styles.mapsData}>
-                    <div className={styles.headDiv}>
-                        <h1 className={styles.mapsName}>Nome</h1>
-                        <h2 className={styles.favoriteIcon}>ICONE</h2>
-                    </div>
-                    <img className={styles.mapsImage} src={"/assets/mapas/WORLDS-EDGE/worlds-edge-official.jpg"} />
-                </div>
-                <div className={styles.mapsData}>
-                    <div className={styles.headDiv}>
-                        <h1 className={styles.mapsName}>Nome</h1>
-                        <h2 className={styles.favoriteIcon}>ICONE</h2>
-                    </div>
-                    <img className={styles.mapsImage} src={"/assets/mapas/BROKEN-MOON/broken-moon-official.jpg"} />
-                </div>
-            </div>
+                <MapsPage />
         </div>
     )
 }
