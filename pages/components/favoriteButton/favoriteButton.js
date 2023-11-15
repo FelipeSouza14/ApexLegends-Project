e50482964f2c182a7e10955676ff8ae303ccd7a3
IsaKaillany <isakaillany123@gmail.com>
2023-11-15 18:43:11 -0300

    update tipo do botão

--- pages/components/favoriteButton/favoriteButton.js
+++ pages/components/favoriteButton/favoriteButton.js
@@ -1,11 +1,11 @@
 import { MdFavoriteBorder } from "react-icons/md";
 
-import styles from './favoriteButton.module.css';
+import styles from "./favoriteButton.module.css";
 
 export function FavoriteButton({ size }) {
     return (
         <div>
-            <button className={styles.favoriteButton}>
+            <button type="submit" className={styles.favoriteButton}>
                 <MdFavoriteBorder size={size} />
             </button>
         </div>
