932f6fb8321743fc89e75e82453ebc01fb89469f
Thamiris Borges <87441572+Thami03@users.noreply.github.com>
2023-11-20 11:37:19 -0300

    adicionando

---
+++ pages/homeScreen/homeNavBar/homeNavBar.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+import styles from "./homeNavBar.module.css";
+
+export function HomeNavBar(){
+    return (
+        <nav className={styles.navBar}>
+            <img className={styles.logoImagem} src={"/assets/logos/logo-sem-fundo.png"} />
+            <div>
+               <Link href="/login"><button className={styles.buttonLogin}>Log in</button></Link>  
+               <Link href="/register"><button className={styles. buttonSignin}>Sign in</button></Link>
+            </div>
+        </nav>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { HomeNavBar } from "./homeScreen/homeNavBar/homeNavBar"
+import { HomeCards } from "./homeScreen/homeCards/homeCards"
+
+import styles from "./styles/index.module.css"
+
+export default function Main() {
+    return(
+        <div className={styles.homePage}>
+            <HomeNavBar />
+            <HomeCards />
+        </div>    
+    )
+} 
