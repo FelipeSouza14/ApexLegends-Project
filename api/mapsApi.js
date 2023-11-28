eed2d0e359e2d1f12d66dd7d4486c8c977942070
IsaKaillany <isakaillany123@gmail.com>
2023-11-27 22:27:37 -0300

    puxando os dados de mapas do BD

--- api/mapsApi.js
+++ api/mapsApi.js
@@ -3,7 +3,7 @@ import db from "../util/firebase";
 
 export async function getMapsData() {
     const firestore = getFirestore(db);
-    const mapsCollection = collection(firestore, 'lendas');
+    const mapsCollection = collection(firestore, 'mapas');
     const mapsSnapshot = await getDocs(mapsCollection);
     const mapsData = mapsSnapshot.docs.map(doc => doc.data());    
     return mapsData;
