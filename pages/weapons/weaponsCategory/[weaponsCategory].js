f1547d06bedddbabdc84f100b0c0f9e8db0c9703
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-02 02:20:22 -0300

    update no css de legendData e tirando comentarios

--- pages/weapons/weaponsCategory/[weaponsCategory].js
+++ pages/weapons/weaponsCategory/[weaponsCategory].js
@@ -25,53 +25,6 @@ export default function WeaponsCategory({ weaponsDetails }) {
                     </div>
                 </div>
             )})}
-            {/* <div className={styles.backDiv}>
-                <div className={styles.frontDiv}>
-                    <img src={weaponsDetails.imagem} />
-                    <div className={styles.divFavoriteButton}>
-                        <p className={styles.nomes}>{weaponsDetails.nome}</p>
-                        <FavoriteButton size={30} />
-                    </div>
-                </div>
-            </div> */}
-
-
-            {/* <div className={styles.backDiv}>
-                <div className={styles.frontDiv}>
-                    <img src={"/assets/armas/AR/VK-47_Flatline.webp"} />
-                    <div className={styles.divFavoriteButton}>
-                        <p className={styles.nomes}>Flatline</p>
-                        <FavoriteButton size={30} />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.backDiv}>
-                <div className={styles.frontDiv}>
-                    <img src={"/assets/armas/AR/HAVOC_Rifle.webp"} />
-                    <div className={styles.divFavoriteButton}>
-                        <p className={styles.nomes}>Havoc</p>
-                        <FavoriteButton size={30} />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.backDiv}>
-                <div className={styles.frontDiv}>
-                    <img src={"/assets/armas/AR/Nemesis_Burst_AR.webp"} />
-                    <div className={styles.divFavoriteButton}>
-                        <p className={styles.nomes}>Nemesis</p>
-                        <FavoriteButton size={30} />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.backDiv}>
-                <div className={styles.frontDiv}>
-                    <img src={"/assets/armas/AR/Hemlok_Burst_AR.webp"} />
-                    <div className={styles.divFavoriteButton}>
-                        <p className={styles.nomes}>Hemlok</p>
-                        <FavoriteButton size={30} />
-                    </div>
-                </div>
-            </div> */}
         </div>
     );
 }
