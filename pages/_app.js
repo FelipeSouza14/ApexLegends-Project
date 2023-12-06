df181b2a28e549430da4386de143a0455a25a33d
Thamiris Borges <87441572+Thami03@users.noreply.github.com>
2023-12-05 23:45:29 -0300

    modificando o arquivo _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import './styles/globals.css';
+import { AuthProvider } from '../context/authContext';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
