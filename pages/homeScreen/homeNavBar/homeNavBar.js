65f7607e24c8b159b703bba3779d91196e6d91a1
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-09 20:40:49 -0300

    adicionando os arquivos de profileDiv e estado

--- pages/homeScreen/homeNavBar/homeNavBar.js
+++ pages/homeScreen/homeNavBar/homeNavBar.js
@@ -1,18 +1,39 @@
 import Link from "next/link";
 import styles from "./homeNavBar.module.css";
 import { IoPersonCircleOutline } from "react-icons/io5";
+import { useAuth } from "../../../context/authContext";
+import { useRouter } from 'next/router';
 
-export function HomeNavBar(){
+
+
+export function HomeNavBar() {
+    const { isLoggedIn } = useAuth();
+    const router = useRouter();
+
+    const logged = () => {
+        if (isLoggedIn) {
+            router.push('/login');
+        }
+        else {
+            return (
+                // className={styles.divProfile}
+                <div className={styles.divProfile}>
+                    <p>sair</p>
+                </div>
+            );
+        }
+    }
+
     return (
         <nav className={styles.navBar}>
-        <img className={styles.logoImagem} src={"/assets/logos/logo-sem-fundo.png"} />
-        <div>
-            <button className={styles.profileButton}>
-                <IoPersonCircleOutline size={45}/>
-            </button>
-           {/* <Link href="/login"><button className={styles.buttonLogin}>Entrar</button></Link>  
+            <img className={styles.logoImagem} src={"/assets/logos/logo-sem-fundo.png"} />
+            <div>
+                <button className={styles.profileButton} onClick={logged}>
+                    <IoPersonCircleOutline size={45} />
+                </button>
+                {/* <Link href="/login"><button className={styles.buttonLogin}>Entrar</button></Link>  
            <Link href="/register"><button className={styles. buttonSignin}>Criar conta</button></Link> */}
-        </div>
-    </nav>
-)
+            </div>
+        </nav>
+    )
 }
---
+++ pages/homeScreen/homeNavBar/profileDiv.js
@@ -0,0 +1,7 @@
+import styles from "";
+
+export default function ProfileDiv () {
+    return (
+
+    );
+}
