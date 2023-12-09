eacc0d3ab04c3f248bd5e3a335676582d50af59d
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-09 18:59:10 -0300

    adicionando o botão de perfil na tela inicial

--- pages/homeScreen/homeNavBar/homeNavBar.js
+++ pages/homeScreen/homeNavBar/homeNavBar.js
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import styles from "./homeNavBar.module.css";
+import { IoPersonCircleOutline } from "react-icons/io5";
 
 export function HomeNavBar(){
     return (
         <nav className={styles.navBar}>
         <img className={styles.logoImagem} src={"/assets/logos/logo-sem-fundo.png"} />
         <div>
-           <Link href="/login"><button className={styles.buttonLogin}>Log in</button></Link>  
-           <Link href="/register"><button className={styles. buttonSignin}>Sign in</button></Link>
+            <button className={styles.profileButton}>
+                <IoPersonCircleOutline size={45}/>
+            </button>
+           {/* <Link href="/login"><button className={styles.buttonLogin}>Entrar</button></Link>  
+           <Link href="/register"><button className={styles. buttonSignin}>Criar conta</button></Link> */}
         </div>
     </nav>
 )
--- pages/legends/legendCard/legendCard.js
+++ pages/legends/legendCard/legendCard.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { getLegendsData } from "../../../api/legendsApi";
-
 import styles from "./legendCard.module.css";
 import { SearchBar } from "../searchBar/searchBar";
 
