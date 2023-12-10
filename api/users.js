61010dd005396b9972ee19696a1ba6b1599b2cec
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-10 01:07:47 -0300

    acessando a API de usuarios

---
+++ api/users.js
@@ -0,0 +1,12 @@
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import db from '../util/firebase';
+
+export async function getUserByEmail(email) {
+    const firestore = getFirestore(db);
+    const usersCollection = collection(firestore, 'usuario');
+    const userQuery = query(usersCollection, where('email', '==', email));
+    const userSnapshot = await getDocs(userQuery);
+
+    const userData = userSnapshot.docs.map(doc => doc.data());
+    return userData; 
+}
