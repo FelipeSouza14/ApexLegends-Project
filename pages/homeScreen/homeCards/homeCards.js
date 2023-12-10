6139bbfdd2451f8fa94e8075e13aea145ce31b8a
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-10 04:24:06 -0300

    atualizando a rota dos cards da HomePage

--- pages/homeScreen/homeCards/homeCards.js
+++ pages/homeScreen/homeCards/homeCards.js
@@ -1,33 +1,45 @@
 import Link from "next/link";
 import styles from "./homeCards.module.css";
+import { useRouter } from 'next/router';
+import { useAuth } from "../../../context/authContext";
+import { useState } from "react";
 
 export function HomeCards() {
+    const { isLoggedIn } = useAuth();
+    const router = useRouter();
+    const [legendsScreen, setLegendsScreen] = useState('/legends/legendsPage');
+    const [weaponsScreen, setWeaponsScreen] = useState('../weapons/weaponsPage');
+    const [mapsScreen, setMapsScreen] = useState('../maps/mapsPage');
+
+    const logged = (screen) => {
+        if (isLoggedIn) {
+            router.push(screen);
+        }
+        else {
+            router.push('/login');
+        }
+    }
+
     return (
         <div className={styles.divCards}>
-            <Link href="/legends/legendsPage">
-                <div className={styles.cardsHomePage}>
-                    <img
-                        className={styles.cardLendasImagem}
-                        src={"/assets/cards/card-lendas.png"}
-                    />
-                </div>
-            </Link>
-            <Link href="../weapons/weaponsPage">
-                <div className={styles.cardsHomePage}>
-                    <img
-                        className={styles.cardLendasImagem}
-                        src={"/assets/cards/card-armas.png"}
-                    />
-                </div>
-            </Link>
-            <Link href="../maps/mapsPage">
-                <div className={styles.cardsHomePage}>
-                    <img
-                        className={styles.cardLendasImagem}
-                        src={"/assets/cards/card-mapas.png"}
-                    />
-                </div>
-            </Link>
+            <div className={styles.cardsHomePage} onClick={() => logged(legendsScreen)}>
+                <img
+                    className={styles.cardLendasImagem}
+                    src={"/assets/cards/card-lendas.png"}
+                />
+            </div>
+            <div className={styles.cardsHomePage} onClick={() => logged(weaponsScreen)}>
+                <img
+                    className={styles.cardLendasImagem}
+                    src={"/assets/cards/card-armas.png"}
+                />
+            </div>
+            <div className={styles.cardsHomePage} onClick={() => logged(mapsScreen)}>
+                <img
+                    className={styles.cardLendasImagem}
+                    src={"/assets/cards/card-mapas.png"}
+                />
+            </div>
         </div>
     );
 }
