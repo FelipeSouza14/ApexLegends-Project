bcacd2fdfba3b53bbec6f4da665bdce586f4f228
IsaKaillany <isakaillany123@gmail.com>
2023-12-11 18:40:53 -0300

    add logOut da conta

--- pages/homeScreen/homeNavBar/profileDiv.js
+++ pages/homeScreen/homeNavBar/profileDiv.js
@@ -1,13 +1,16 @@
 import styles from "./profileDiv.module.css";
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { IoIosCloseCircle } from "react-icons/io";
+import { useAuth } from "../../../context/authContext";
 
 export function ProfileDiv ({ onClose }) {
+    const { logOut } = useAuth();
+
     return (
         <div className={styles.profileDiv}>
             <IoPersonCircleOutline size={45} className={styles.profileIcon}/>
             <p className={styles.paragraph}>MINHA COLEÇÃO</p>
-            <p className={styles.paragraph}>SAIR</p>
+            <button className={styles.paragraph} onClick={logOut}>SAIR</button>
             <button onClick={onClose} className={styles.closeButton}>
                 <IoIosCloseCircle size={25}/>
             </button>
