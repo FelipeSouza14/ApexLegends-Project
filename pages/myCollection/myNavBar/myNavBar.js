3553878e4b0efb376323ccb83615ffa4911655bf
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-12 04:07:48 -0300

    melhorando o css

--- pages/myCollection/myNavBar/myNavBar.js
+++ pages/myCollection/myNavBar/myNavBar.js
@@ -1,6 +1,12 @@
+import { getUserByEmail } from "../../../api/usersApi";
+import { auth } from "../../../util/firebase";
+// import { auth } from "../../../util/firebase";
+// import { getDocs, collection, query, where } from "firebase/firestore";
 import styles from "./myNavBar.module.css";
 
-export default function MyNavBar({stateAuth, statePage}) {
+
+
+export default function MyNavBar({ stateAuth, statePage }) {
 
     // const mudar = (numb) => {
     //     if (stateAuth && numb === 1) {
@@ -13,14 +19,33 @@ export default function MyNavBar({stateAuth, statePage}) {
 
     const mudarPagina = (pagina) => {
         if (stateAuth) statePage(pagina);
-      };
+    };
 
     // const mud = () => {
     //     if (stateAuth) {
     //         funcWeapons('showWeapons');
     //     }
     // }
+    // const loadUser = async () =>{
+    //     const legendsCollection = collection(db, "usuario")
+    //     const legendsQuery = query(legendsCollection, where('email', '==', auth.currentUser.email))
+    //     const querySnapshot = await getDocs(legendsQuery);
+    //     return querySnapshot.data()
+    // }
 
+    // const teste = (stateAu) =>{
+    //     if (stateAu) {
+    //         const userData = getUserByEmail(stateAu.email);
+    //         console.log(userData)
+    //         return (
+    //             <p className={styles.userCollection}>Coleção de {userData.name}</p>
+    //         );
+    //     }
+    // }
+
+    // const userData = getUserByEmail(auth.currentUser.email);
+    // let getUserData = userData(auth.currentUser.email);
+    // console.log(userData)
     return (
         <nav className={styles.myNavBar}>
             <img
@@ -32,6 +57,7 @@ export default function MyNavBar({stateAuth, statePage}) {
                 <button className={styles.myNavButton} onClick={() => mudarPagina(2)}>Armas</button>
                 <button className={styles.myNavButton} onClick={() => mudarPagina(3)}>Mapas</button>
             </div>
+            {/* {teste(stateAuth)} */}
         </nav>
     );
 }
