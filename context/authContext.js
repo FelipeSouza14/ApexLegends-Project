45431e5ba417bec7b8091b96dfa39bdc09463193
IsaKaillany <isakaillany123@gmail.com>
2023-12-12 21:34:01 -0300

    update detalhes

--- context/authContext.js
+++ context/authContext.js
@@ -13,7 +13,7 @@ export const AuthProvider = ({ children }) => {
 
     const logOut = () => {
         setIsLoggedIn(false);
-        console.log("Deslogado");
+        alert("Você foi deslogado!");
     };
 
     return (
--- pages/myCollection/myLoadDatas/myLoadLegends.js
+++ pages/myCollection/myLoadDatas/myLoadLegends.js
@@ -4,7 +4,6 @@ import { db, auth } from "../../../util/firebase";
 import { deleteDoc, doc, getDocs, collection, query, where } from "firebase/firestore";
 import { MdDelete } from "react-icons/md";
 
-
 export default function MyLoadLegendsData() {
     const [myLegendsData, setMyLegendsData] = useState([]);
     let hasUser = true;
@@ -12,66 +11,72 @@ export default function MyLoadLegendsData() {
     try {
         const loadData = async () => {
             if (auth.currentUser) {
-                let newData = []
-                const legendsCollection = collection(db, "lendasFav")
-                const legendsQuery = query(legendsCollection, where('email', '==', auth.currentUser.email))
+                let newData = [];
+                const legendsCollection = collection(db, "lendasFav");
+                const legendsQuery = query(
+                    legendsCollection,
+                    where("email", "==", auth.currentUser.email)
+                );
                 const querySnapshot = await getDocs(legendsQuery);
 
                 querySnapshot.forEach((doc) => {
-
                     let docData = doc.data();
-                    newData.push({ image: docData.image, name: docData.name, docId: doc.id });
-                })
-                setMyLegendsData(newData)
-
+                    newData.push({
+                        image: docData.image,
+                        name: docData.name,
+                        docId: doc.id,
+                    });
+                });
+                setMyLegendsData(newData);
+            } else {
+                hasUser = false;
             }
-            else {
-                hasUser = false
-            }
-        }
+        };
         if (myLegendsData.length == 0) {
-            loadData()
+            loadData();
         }
     } catch (error) {
-        console.log("nao foi possivel continuar")
+        alert("Não foi possível continuar");
+        console.log("Não foi possível continuar");
     }
 
     const deleteFav = async (props) => {
-        const { id } = props
+        const { id } = props;
 
         try {
             if (auth.currentUser) {
-                console.log('entrou no delete')
-                console.log(id);
+                // console.log('entrou no delete');
+                // console.log(id);
 
                 await deleteDoc(doc(db, "lendasFav", id));
-                setMyLegendsData(myLegendsData.filter((i) => i.docId !== id))
-            }
-            else {
-                console.log("usuario nao encontrado")
+                setMyLegendsData(myLegendsData.filter((i) => i.docId !== id));
+            } else {
+                alert("Usuário não encontrado!");
             }
         } catch (error) {
-            console.log("nao foi possivel continuar")
+            alert("Não foi possível continuar");
+            console.log("Não foi possível continuar");
         }
-    }
+    };
     return (
         <div className={styles.divLegends}>
-            {
-                myLegendsData.map((legend, index) => (
-                    <figure className={styles.figureContainer} key={index}>
-                        <img
-                            src={legend.image}
-                            width={350}
-                            alt={`Imagem de ${legend.nome}`}
-                        />
-                        <figcaption>
-                            {legend.name}
-                            <div className={styles.deleteButton}>
-                                <MdDelete onClick={() => deleteFav({ id: legend.docId })} />
-                            </div>
-                        </figcaption>
-                    </figure>
-                ))}
+            {myLegendsData.map((legend, index) => (
+                <figure className={styles.figureContainer} key={index}>
+                    <img
+                        src={legend.image}
+                        width={350}
+                        alt={`Imagem de ${legend.nome}`}
+                    />
+                    <figcaption>
+                        {legend.name}
+                        <div className={styles.deleteButton}>
+                            <MdDelete
+                                onClick={() => deleteFav({ id: legend.docId })}
+                            />
+                        </div>
+                    </figcaption>
+                </figure>
+            ))}
         </div>
     );
 }
